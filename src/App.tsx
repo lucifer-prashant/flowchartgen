1eb5c72671435167d746d9d616369a364e52c754
lucifer-prashant <22051530@kiit.ac.in>
2026-04-24 20:16:54 +0530

    analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import { Suspense } from "react";
-import { Routes, Route } from "react-router-dom";
-import { Toaster } from "sonner";
-import Home from "./components/home";
+import { Suspense } from "react"
+import { Routes, Route } from "react-router-dom"
+import { Toaster } from "sonner"
+import { Analytics } from "@vercel/analytics/react"
+import Home from "./components/home"
 
 function App() {
-  return (
-    <Suspense fallback={<p>Loading...</p>}>
-      <>
-        <Toaster theme="dark" position="top-right" richColors />
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </>
-    </Suspense>
-  );
+	return (
+		<Suspense fallback={<p>Loading...</p>}>
+			<>
+				<Toaster theme="dark" position="top-right" richColors />
+				<Routes>
+					<Route path="/" element={<Home />} />
+				</Routes>
+				<Analytics />
+			</>
+		</Suspense>
+	)
 }
 
-export default App;
+export default App
